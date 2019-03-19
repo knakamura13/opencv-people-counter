08f2f4942456247216098e5f792a0efb389f1648
Kyle Nakamura <kjnakamura@apu.edu>
2019-03-19 12:26:52 -0700

    Add -h parameter for FirstDetection.py

--- FirstDetection.py
+++ FirstDetection.py
@@ -9,15 +9,20 @@ def main(argv):
 	outputfile = ''
 
 	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+		opts, args = getopt.getopt(argv,"hi:o:",["inputfile=","outputfile="])
 	except getopt.GetoptError:
 		print('Try using the syntax: test.py -i <inputfile> -o <outputfile>')
 		sys.exit(2)
 
 	for opt, arg in opts:
-		if opt in ("-i", "--ifile"):
+		if opt in ("-h", "--help"):
+			print('Usage:\t-h, --help:\t\t Get help')
+			print('\t-i, --inputfile:\t Select input file')
+			print('\t-o, --outputfile:\t Select output file')
+			sys.exit(2)
+		elif opt in ("-i", "--inputfile"):
 			inputfile = arg
-		elif opt in ("-o", "--ofile"):
+		elif opt in ("-o", "--outputfile"):
 			outputfile = arg
 
 	execution_path = os.getcwd()
@@ -27,6 +32,19 @@ def main(argv):
 	detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 	detector.loadModel(detection_speed="fast")
 
+	# Update the parameters to choose which objects are detected.
+	'''
+	Possible object types:
+		person, bicycle, car, motorcycle, airplane,
+        bus, train, truck, boat, traffic_light, fire_hydrant, stop_sign,
+        parking_meter, bench, bird, cat, dog, horse, sheep, cow, elephant, bear, zebra,
+        giraffe, backpack, umbrella, handbag, tie, suitcase, frisbee, skis, snowboard,
+        sports_ball, kite, baseball_bat, baseball_glove, skateboard, surfboard, tennis_racket,
+        bottle, wine_glass, cup, fork, knife, spoon, bowl, banana, apple, sandwich, orange,
+        broccoli, carrot, hot_dog, pizza, donot, cake, chair, couch, potted_plant, bed,
+        dining_table, toilet, tv, laptop, mouse, remote, keyboard, cell_phone, microwave,
+        oven, toaster, sink, refrigerator, book, clock, vase, scissors, teddy_bear, hair_dryer, toothbrush
+	'''
 	custom_objects = detector.CustomObjects(car=True, person=True)
 
 	detections = detector.detectCustomObjectsFromImage(
