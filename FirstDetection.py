1137ca20fa104b0698db524641049ac1773ddab8
Kyle Nakamura <kjnakamura@apu.edu>
2019-03-19 12:14:41 -0700

    Finish FirstDetection.py with parameters for the script.

--- FirstDetection.py
+++ FirstDetection.py
@@ -1,13 +1,39 @@
+#!/usr/bin/python
+
 from imageai.Detection import ObjectDetection
 import os
+import sys, getopt
 
-execution_path = os.getcwd()
+def main(argv):
+	inputfile = ''
+	outputfile = ''
 
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
-detector.loadModel(detection_speed="fast")
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print('Try using the syntax: test.py -i <inputfile> -o <outputfile>')
+		sys.exit(2)
 
-custom_objects = detector.CustomObjects(car=True)
+	for opt, arg in opts:
+		if opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
 
-detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=os.path.join(execution_path , "cars.jpg"), output_image_path=os.path.join(execution_path , "carsnew.jpg"))
+	execution_path = os.getcwd()
+
+	detector = ObjectDetection()
+	detector.setModelTypeAsRetinaNet()
+	detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+	detector.loadModel(detection_speed="fast")
+
+	custom_objects = detector.CustomObjects(car=True, person=True)
+
+	detections = detector.detectCustomObjectsFromImage(
+		custom_objects=custom_objects,
+		input_image=inputfile,
+		output_image_path=outputfile,
+	)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
